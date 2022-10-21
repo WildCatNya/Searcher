a4ca304166cd11ef0c834eaf3ac65b5429335840
Кирилл Кудей <kudey_kir@mail.ru>
2022-10-21 13:26:40 +0700

    Начал реализовывать паттерн состояния

---
+++ StudentSearch/State/IState.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace Searcher.State;
+
+public interface IState<PersonType>
+{
+    IEnumerable<PersonType> Search();
+    void Clear();
+}
---
+++ StudentSearch/State/StudentState.cs
@@ -0,0 +1,27 @@
+﻿using Searcher.Models;
+using System.Collections.Generic;
+
+namespace Searcher.State;
+
+public class StudentState : IState<Student>
+{
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+    public IEnumerable<Student> Search()
+    {
+        throw new System.NotImplementedException();
+    }
+}
+public class TeacherState : IState<Teacher>
+{
+    public void Clear()
+    {
+        throw new System.NotImplementedException();
+    }
+    public IEnumerable<Teacher> Search()
+    {
+        throw new System.NotImplementedException();
+    }
+}
